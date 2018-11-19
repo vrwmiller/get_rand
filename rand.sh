c57ffb64ee78c0304b315c2a086ed77ee699df13
Vincent Miller <vrwmiller@gmail.com>
2018-11-19 21:48:02 +0000

    Initial commit.

---
+++ rand.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+MAXNUM=65534
+
+get_rand() {
+
+  local bound=$1
+  local i=65535
+
+  while [ ! $i -lt $bound ] || [ $i -gt $MAXNUM ]; do
+    i=`cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2`
+  done
+
+  echo "$i"
+
+}
+
+KEY=`get_rand 59`
+echo "${KEY}"
